25904bc87e57f4c4de55df10fe4fa3796cb01726
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-08-28 13:54:51 +0800

    compile success

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,19 @@
 use futures::StreamExt;
 use packet::ip::Packet;
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::future::Future;
 use std::io::{self, Read};
 use std::net::SocketAddr;
 use std::pin::Pin;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 use tokio::net::UdpSocket;
+use tokio::sync::broadcast;
+use tokio::task::JoinHandle;
+use anyhow;
+
 
 extern crate tun;
 use tun::AsyncDevice;
@@ -22,15 +27,16 @@ pub struct Server {
 }
 
 struct ClientNode {
-    iface: AsyncDevice,
     verified: bool,
+    thread: Option<JoinHandle<()>>,
 }
 
 impl Server {
-    async fn run(self) -> Result<(), io::Error> {
+    async fn run(self) -> anyhow::Result<()> {
         let mut clients = HashMap::new();
 
         let mut buf = vec![0; 0x1_0000];
+        let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(10);
 
         loop {
             let (data_size, addr) = self.socket.recv_from(&mut buf).await?;
@@ -38,10 +44,41 @@ impl Server {
                 continue;
             }
 
-            let client_node = clients.entry(addr).or_insert_with(|| ClientNode {
-                iface: Server::create_interface(),
-                verified: false,
+            let client_node = clients.entry(addr).or_insert_with(|| {
+                Arc::new(Mutex::new(ClientNode {
+                    verified: false,
+                    thread: None,
+                }))
             });
+
+            let node2 = client_node.clone();
+
+            let mut client_node = client_node.lock().unwrap();
+
+            if let None = client_node.thread {
+                let thread = tokio::spawn(async move {
+                    let iface = Server::create_interface();
+
+                    let mut ll = node2;
+                    // let dev = Server::create_interface();
+                    let mut stream = iface.into_framed();
+
+                    while let Some(packet) = stream.next().await {
+                        match packet {
+                            Ok(pkt) => {
+                                println!("pkt: {:#?}", Packet::unchecked(pkt.get_bytes()))
+                            }
+                            Err(err) => panic!("TUN interface error: {:?}", err),
+                        }
+                    }
+
+                });
+                client_node.thread = Some(thread);
+            }
+
+            let tx = tx.clone();
+            let mut rx = tx.subscribe();
+    
             if !client_node.verified {
                 if Server::verify_package(&buf, &self.secret) {
                     client_node.verified = true;
@@ -58,9 +95,9 @@ impl Server {
                 self.socket.send_to(&buf[..data_size], addr).await?;
                 continue;
             }
-
             // write package to TUN interface
         }
+        Ok(())
     }
 
     fn create_interface() -> AsyncDevice {
@@ -102,7 +139,7 @@ impl Server {
 }
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "0.0.0.0:8088".to_string());
@@ -121,53 +158,3 @@ async fn main() -> io::Result<()> {
 
     Ok(())
 }
-
-/*
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let mut config = tun::Configuration::default();
-
-    config
-        .address((10, 0, 0, 2))
-        .netmask((255, 255, 255, 0))
-        .destination((10, 0, 0, 1))
-        .up();
-
-    #[cfg(target_os = "linux")]
-    config.platform(|config| {
-        config.packet_information(true);
-    });
-
-    let mut dev = tun::create_as_async(&config).unwrap();
-    let sock = UdpSocket::bind("0.0.0.0:8082").await?;
-    let remote_addr = "127.0.0.1:8081".parse::<SocketAddr>().unwrap();
-    sock.connect(remote_addr).await?;
-    let mut outlet = UdpOutlet::new(sock);
-    println!("started");
-    let (u, d) = tokio::io::copy_bidirectional(&mut outlet, &mut dev).await?;
-    println!("up: {}, down: {}", u, d);
-    Ok(())
-}
-// */
-
-/*
-fn main() {
-    let mut config = tun::Configuration::default();
-    config.address((10, 0, 0, 1))
-           .netmask((255, 255, 255, 0))
-           .up();
-
-    #[cfg(target_os = "linux")]
-    config.platform(|config| {
-        config.packet_information(true);
-    });
-
-    let mut dev = tun::create(&config).unwrap();
-    let mut buf = [0; 4096];
-
-    loop {
-        let amount = dev.read(&mut buf).unwrap();
-        println!("{:?}", &buf[0 .. amount]);
-    }
-}
-// */
